83a4202fd0d1106cf17dc1e6e52c4450145bfd64
RealND7 <maxicanapark@gmail.com>
2025-12-24 16:37:58 +0900

    feat: Upgrade to Java 21 and improve Project List UI

--- backend/src/main/java/com/editus/backend/domain/auth/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.editus.backend.domain.auth.controller;
-import org.springframework.web.bind.annotation.*;
-@RestController
-@RequestMapping("/api/auth")
-public class UserController {
-    // TODO: 이세종 - 회원가입/내 정보 조회 API 구현
-}
