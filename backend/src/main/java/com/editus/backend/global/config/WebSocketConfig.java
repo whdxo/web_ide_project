315ebb9f7d1ed4339257f964a902a9ba48f1fdac
lsj0416 <hhh3915@gmail.com>
2025-12-29 10:54:02 +0900

    fix: WebSocketConfig 수정

--- backend/src/main/java/com/editus/backend/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/editus/backend/global/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/ws-chat")
                 .setAllowedOriginPatterns("*");
     }
 
@@ -55,8 +55,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
                             // Create Authentication object
                             Authentication authentication = new org.springframework.security.authentication.UsernamePasswordAuthenticationToken(
-                                email, null, java.util.Collections.emptyList()
-                            );
+                                    email, null, java.util.Collections.emptyList());
 
                             SecurityContextHolder.getContext().setAuthentication(authentication);
                             accessor.setUser(authentication);
